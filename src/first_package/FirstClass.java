172f6323992d53aff4b845da15c0692e94191d44
Tacettin Kot <tacettinkot@gmail.com>
2020-08-11 23:19:27 -0700

    Last commit from local master branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -6,5 +6,8 @@ public class FirstClass {
 		System.out.println("Our first commit from local repo");
 		System.out.println("Our second commit from local repository");
 		System.out.println("Now I am on my tacettin branch");
+		System.out.println("I want to have conflict");
+		
+		System.out.println("I want to have second conflict");
 	}
 }
